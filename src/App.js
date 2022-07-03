ea6ec92dd35e55be39075c6d6894e3bb6359cca7
Afzaal Shoukat <afzaalsatti74@gmail.com>
2022-07-03 13:04:41 +0200

    testing server response

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <button onClick={()=>{
+
+let serverLink="https://wtc.tu-chemnitz.de/krb/saml2/idp/SSOService.php?target=https://62c1768e9c3edd79197a71fb--symphonious-youtiao-4e8c91.netlify.app/shibboleth/&providerId=https://62c1768e9c3edd79197a71fb--symphonious-youtiao-4e8c91.netlify.app/"
+  axios.get(serverLink, {
+    
+  }).then((result) => {
+      console.log("resssssssssss",result)
+  
+     
+  }).catch((err) => {
+    swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong while fetching requests!',
+    
+    })
+     
+  });;
+
+   }}>Test</button>
     </div>
   );
 }
